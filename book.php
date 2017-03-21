0a5e915769201a82d498bddb5ef427173cde1eeb
João Pedro de Souza Coutinho <jpscoutinho@inf.ufrgs.br>
2017-03-21 15:05:55 -0300

    lots of changes
    1

---
+++ book.php
@@ -0,0 +1,4 @@
+<?php
+
+	$conn = mysql_connect('localhost', 'root', 'vertrigo');
+	$db = mysql_select_db('jaquelinegauer')
---
+++ db.php
@@ -0,0 +1,8 @@
+<?php
+
+	$conn = mysql_connect('localhost', 'root', 'vertrigo');
+	$db = mysql_select_db('jaquelinegauer');
+
+
+
+?>
