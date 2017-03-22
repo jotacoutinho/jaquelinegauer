2327fe88f7e0891bd7d3a5c55cdda08376d2fe15
João Pedro de Souza Coutinho <jpscoutinho@inf.ufrgs.br>
2017-03-22 14:48:31 -0300

    exception
    asdds

--- book.php
+++ book.php
@@ -1,14 +1,22 @@
 <?php
 	
-		include_once('db.php');
+		mysql_connect("localhost", "root", "vertrigo") or die(mysql_error());
+   		mysql_select_db("jaquelinegauer") or die(mysql_error());
 
 		$id = $_POST['id'];
 		$name = $_POST['name'];
 		$email = $_POST['email'];
 
-		if(mysql_query("INSERT INTO clients VALUES('$id', '$name', '$email'"))
-			echo "SUCCESS";
-		else
-			echo "ERROR";
+		try
+		{
+			$sql = "INSERT INTO clients VALUES('$id', '$name', '$email'";
+			$result = mysql_query($sql) or die(mysql_error());	
+			echo "1";
+		}
+		catch(Exception $ex)
+		{
 
+			echo "0";
+		}
+		
 ?>
